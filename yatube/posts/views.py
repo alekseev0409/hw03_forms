c3dfc37ba240bf9ff7943bf9020485e11531fb13
Алексей Алексеев <alekseev0409@gmail.com>
2023-02-19 19:58:09 +0300

    finish him4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    post_list = Post.objects.select_related('group','author')
+    post_list = Post.objects.select_related('group', 'author')
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
